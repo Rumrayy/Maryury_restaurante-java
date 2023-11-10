58bd8a598dc069560b62ed82c20bfc4c3e402943
Rumrayy <132438472+Rumrayy@users.noreply.github.com>
2023-11-09 20:30:19 -0600

    modificacion login

--- src/main/webapp/javascript/login.js
+++ src/main/webapp/javascript/login.js
@@ -1,44 +1,41 @@
-async function loginUser(email, password) {
-  event.preventDefault();
-  // Define la URL de la API y los datos a enviar
-  const url = 'http://localhost:8090/reserva/api/user/login';
-  const data = {
-    email: email,
-    password: password
-  };
+function loginUser() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-  try {
-    // Envía una solicitud POST a la API
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)  // Convierte los datos a una cadena JSON
+    // Datos a enviar
+    var data = {
+        email: email,
+        password: password
+    };
+
+    // URL de la API
+    var apiURL = 'http://localhost:8090/reserva/api/user/login';
+
+    // Enviar solicitud a la API
+    fetch(apiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => {
+        if (!response.ok) {
+            // Si el estado no es 200 OK, se maneja como un inicio de sesión fallido.
+            throw new Error('Inicio de sesión fallido');
+        }
+        return response.json(); // Asumimos que la API siempre responde con JSON.
+    })
+    .then(data => {
+        // Asegúrate de que esta lógica coincide con la estructura de la respuesta de tu API.
+        if (data.message && data.message === 'Login exitoso') {
+            window.location.href = 'reservarmesa.jsp';
+        } else {
+            alert('Correo electrónico o contraseña incorrectos.');
+        }
+    })
+
+    .catch((error) => {
+        console.error('Error:', error);
     });
-
-    // Verifica si la respuesta es exitosa
-    if (response.ok) {
-      // Obtén y retorna el token JWT del encabezado de autorización
-      const jwtToken = response.headers.get('Authorization');
-      return jwtToken;
-    } else {
-      // Si la respuesta no es exitosa, lanza un error
-      throw new Error('Login failed: ' + response.statusText);
-    }
-  } catch (error) {
-    // Captura y maneja cualquier error que ocurra
-    console.error('Error:', error.message);
-  }
-  document.getElementById('loginForm').addEventListener('submit', async function(event)) {
-
-
-          const email = document.getElementById('email').value;
-          const password = document.getElementById('password').value;
-
-          const jwtToken = await loginUser(email, password);
-          if (jwtToken) {
-              console.log('Login successful, JWT Token:', jwtToken);
-          }
-  }
 }
